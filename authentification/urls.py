72399c04259eb8dfacd94642cffdb4ccf681a458
ahmedbrh <brhiliahmed@gmail.com>
2021-03-06 02:20:14 +0100

    starting to setup ajax validation username in register page

--- authentification/urls.py
+++ authentification/urls.py
@@ -1,9 +1,13 @@
 # import views
-from .views import RegistrationView
+from .views import RegistrationView, usernameValidationView
 from django.urls import path
+
+#????? why ? why is not working ?? to see closely ?? fix this later
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
 
     path('register', RegistrationView.as_view(), name="register"),
-    path('login', RegistrationView.as_view(), name="login")
+    path('login', RegistrationView.as_view(), name="login"),
+    path('validate-username', csrf_exempt(usernameValidationView.as_view()), name="validate-username")
 ]
--- authentification/views.py
+++ authentification/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views import View
-
-
+import json
+from  django.http import JsonResponse
 # Create your views here.
+
+
+class usernameValidationView(View):
+    # post request
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if str(username).isalnum():
+            return JsonResponse({"username_error" : "le nom d'utilisateur ne doit pas contenir des caracteres "}, status=400)
+            return JsonResponse({"username_valid": True})
+
 
 
 class RegistrationView(View):
