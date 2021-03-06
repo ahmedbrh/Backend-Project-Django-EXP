29a8987660422893a1083a3914a5f864bafa4ead
ahmedbrh <brhiliahmed@gmail.com>
2021-03-06 01:02:46 +0100

    fix extends &  adding bootstrap & clearing code

--- authentification/urls.py
+++ authentification/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
 
-    path('register', RegistrationView.as_view(), name="register")
+    path('register', RegistrationView.as_view(), name="register"),
+    path('login', RegistrationView.as_view(), name="login")
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('expenses.urls')),
-    path('authentification',include('authentification.urls')),
+    path('authentification/',include('authentification.urls')),
     path('admin/', admin.site.urls)
 
 ]
